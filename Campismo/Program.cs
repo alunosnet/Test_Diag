1a7ac29b607fd617a54c143a9183cc645f23e757
Paulo Ferreira <alunosnet@gmail.com>
2018-09-20 22:23:17 +0100

    Atualizado outro ficheiro

--- Campismo/Program.cs
+++ Campismo/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// Resolução do teste de avaliação diagnóstica
+/// </summary>
 namespace Campismo
 {
     class Program
